25de5528233a7199f2c0223866ac8c00fe42106f
bysking <1982287730@qq.com>
2019-08-03 23:56:35 +0800

    feat: 添加分页功能和路由修复

--- admin/src/components/CategoryList.vue
+++ admin/src/components/CategoryList.vue
@@ -1,40 +1,61 @@
 <template>
     <div class="hello">
         <h1>分类列表</h1>
-        <el-table
+        <div class="table">
+            <el-table
             :data="tableData"
             border
             style="width: 100%">
-
-            <el-table-column
-                align="center"
-                prop="_id"
-                label="id"
-                width="250">
-            </el-table-column>
-            <el-table-column
-                align="center"
-                prop="name"
-                label="分类名称"
-                width="120">
-            </el-table-column>
-            <el-table-column
-                align="center"
-                prop="parentname"
-                label="父级分类"
-                width="120">
-            </el-table-column>
-            <el-table-column
-                align="center"
-                label="操作"
-                width="150">
-                <template slot-scope="scope">
-                    <el-button @click="seeIt(scope.row)" type="text" size="small">查看</el-button>
-                    <el-button @click="editIt(scope.row)" type="text" size="small">编辑</el-button>
-                    <el-button @click="deleteIt(scope.row)" type="text" size="small">删除</el-button>
-                </template>
-            </el-table-column>
-        </el-table>
+                <el-table-column
+                type="index"
+                width="50">
+                </el-table-column>
+                <el-table-column
+                    align="center"
+                    prop="_id"
+                    label="id"
+                    width="250">
+                </el-table-column>
+                <el-table-column
+                    align="center"
+                    prop="name"
+                    label="分类名称"
+                    width="120">
+                </el-table-column>
+                <el-table-column
+                    align="center"
+                    prop="parentname"
+                    label="父级分类"
+                    width="120">
+                </el-table-column>
+                <el-table-column
+                    align="center"
+                    label="操作"
+                    width="150">
+                    <template slot-scope="scope">
+                        <el-button @click="seeIt(scope.row)" type="text" size="small">查看</el-button>
+                        <el-button @click="editIt(scope.row)" type="text" size="small">编辑</el-button>
+                        <el-button @click="deleteIt(scope.row)" type="text" size="small">删除</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </div>
+        <el-col :span ="24">
+            <el-item>
+                <div class="page">
+                    <el-pagination
+                    @size-change="handleSizeChange"
+                    @current-change="handleCurrentChange"
+                    :current-page="myPage.current"
+                    :page-sizes="myPage.sizes"
+                    :page-size="myPage.pageSize"
+                    :layout="myPage.layout"
+                    :total="myPage.total">
+                </el-pagination> 
+                </div>
+            </el-item>
+        </el-col>
+            
     </div>
 </template>
 
@@ -43,7 +64,15 @@ export default {
   name: 'categorylist',
   data () {
     return {
-        tableData: []
+        tableData: [],
+        allData:[],
+        myPage:{
+            current: 1,
+            sizes: [5,10,15,20],
+            total: 0,
+            pageSize: 5,
+            layout:'total, sizes, prev, pager, next, jumper'
+        }
     }
   },
     methods: {
@@ -81,7 +110,35 @@ export default {
         async fetch () {
             const res = await this.$http.get('/rest/categories')
             console.log(res.data)
-            this.tableData = res.data
+            this.tableData = []
+            this.allData = res.data
+            this.myPage.total = this.allData.length
+             for(let i = 0; i < this.myPage.pageSize; i++){
+                this.tableData.push(this.allData[i])
+            }
+
+        }
+        , handleSizeChange(val) {
+            console.log(`每页 ${val} 条`);
+            this.myPage.pageSize = val;
+            this.myPage.current = 1;
+            this.tableData = [];
+            for(let i = 0; i < this.myPage.pageSize; i++){
+                if(this.allData[i]){
+                    this.tableData.push(this.allData[i])
+                }
+                
+            }
+        },
+        handleCurrentChange(val) {
+            console.log(`当前页: ${val}`);
+            this.tableData = [];
+            this.myPage.current = val;
+            for(let i = this.myPage.pageSize*(val-1); i < this.myPage.pageSize*val; i++){
+                if(this.allData[i]){
+                    this.tableData.push(this.allData[i])
+                }
+            }
         }
     },
     created () {
@@ -96,4 +153,13 @@ export default {
     width: 200px;
     min-height: 400px;
   }
+  .table{
+      width: 100%;
+      height: 500px;
+      overflow: scroll;
+    }
+    .page{
+        text-align: right;
+        margin-top: 10px;
+    }
 </style>
--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -4,19 +4,21 @@
 // 小技巧，导出一个函数，他接受一个app，类似代理
 
 module.exports = app => {
+
     const express = require('express')
     // const router = express.Router() // express的子路由
+
     const router = express.Router({
         mergeParams: true // 合并参数 app.use('/admin/api/rest/:resource',router)
     }) 
 
-    // // 导入模型Category.js
-    // const Category = require('../../models/Category')
-    router.get('/test', async (req,res) => {
-        // 数据来源是req.body因为是请求，所以要等待，前边加await，函数定义为async，
+    // 导入模型Category.js
+    const Category = require('../../models/Category')
+
+    router.get('/test', async (req,res) => {    
+        // 数据来源是req.body因为是请求，所以要等待，前边加await，函数定义为async,
         // 还需要在服务端主文件里面添加中间件 app.use(express.json())
         // const model = await Category.create(req.body)
-        console.log('get后台/test')
         const modelName = require('inflection').classify(req.params.resource)
         res.send(modelName)
         // console.log(model)
@@ -34,13 +36,14 @@ module.exports = app => {
         res.send(item)
     })
 
-//  获取列表
+    //  获取列表
     router.get('', async (req,res) => {
         // 从数据库查询结果，限制查询条数10条
         // console.log('进入/list')
         const modelName = require('inflection').classify(req.params.resource)
         const model = require(`../../models/${modelName}`)
-        const list = await model.find().limit(10)
+        // const list = await model.find().limit(10)
+        const list = await model.find()
         res.send(list)
     })
 
@@ -53,6 +56,7 @@ module.exports = app => {
         const item = await model.findById(req.params.id)
         res.send(item)
     })
+
     //  修改指定id的数据
     router.put('/:id', async (req,res) => {
         // 从数据库查询结果，限制查询条数10条
@@ -62,6 +66,7 @@ module.exports = app => {
         const item = await model.findByIdAndUpdate(req.params.id,req.body)
         res.send(item)
     })
+
     //  删除指定id的数据
     router.delete('/:id', async (req,res) => {
         // 从数据库查询结果，限制查询条数10条
@@ -71,4 +76,6 @@ module.exports = app => {
         const item = await model.findByIdAndDelete(req.params.id)
         res.send(item)
     })
+
+    app.use('/admin/api/rest/:resource',router)
 }
